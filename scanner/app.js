597a950263ee4008bcbea748b5f1c036ef1270e2
Isaac (AplAddict) <izip14@gmail.com>
2023-05-23 15:41:27 -0500

    Fixed

--- scanner/app.js
+++ scanner/app.js
@@ -10,24 +10,23 @@ var app = new Vue({
     var self = this;
     self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
     self.scanner.addListener('scan', function (content, image) {
-      if (content.length = 10) {
-        self.scans.unshift({ date: +(Date.now()), content: content });
-        fetch("https://hook.us1.make.com/tfme118y55za9wgcebxwau5u8jy3664p", {
-          method: "POST",
-          body: JSON.stringify({
-            Note: "VIN:" + content
-          }),
-          headers: {
-            "Content-type": "application/json; charset=UTF-8"
-          }
-        });
-      }
-      if ((content.split("-")[0].length = 7) && (content.split("-")[1].length = 2) && (content.split("-")[2].length = 1)) {
+      if ((content.split("-")[0].length == 7) && (content.split("-")[1].length == 2) && (content.split("-")[2].length == 1)) {
         self.scans.unshift({ date: +(Date.now()), content: content });
         fetch("https://hook.us1.make.com/tfme118y55za9wgcebxwau5u8jy3664p", {
           method: "POST",
           body: JSON.stringify({
             Note: content + ": " + prompt('Enter +/- and then the number of part ' + content + ' you would like to add/subtract from inventory.', '')
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          }
+        });
+      } else if (content.length == 10) {
+        self.scans.unshift({ date: +(Date.now()), content: content });
+        fetch("https://hook.us1.make.com/tfme118y55za9wgcebxwau5u8jy3664p", {
+          method: "POST",
+          body: JSON.stringify({
+            Note: "VIN:" + content
           }),
           headers: {
             "Content-type": "application/json; charset=UTF-8"
