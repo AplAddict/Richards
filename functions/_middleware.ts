dd3c53830d276b128c02d7f3398106ff4976f6bb
Isaac (AplAddict) <izip14@gmail.com>
2024-04-02 17:29:36 -0500

    Password

---
+++ functions/_middleware.ts
@@ -0,0 +1,34 @@
+// functions/_middleware.ts
+
+import { CFP_ALLOWED_PATHS } from './constants.ts';
+import { getCookieKeyValue } from './utils.ts';
+import { getTemplate } from './template.ts';
+
+export async function onRequest(context: {
+  request: Request;
+  next: () => Promise<Response>;
+  env: { CFP_PASSWORD?: string };
+}): Promise<Response> {
+  const { request, next, env } = context;
+  const { pathname, searchParams } = new URL(request.url);
+  const { error } = Object.fromEntries(searchParams);
+  const cookie = request.headers.get('cookie') || '';
+  const cookieKeyValue = await getCookieKeyValue(env.CFP_PASSWORD);
+
+  if (
+    cookie.includes(cookieKeyValue) ||
+    CFP_ALLOWED_PATHS.includes("richardsbodyshop.com") ||
+    !env.CFP_PASSWORD
+  ) {
+    // Correct hash in cookie, allowed path, or no password set.
+    // Continue to next middleware.
+    return await next();
+  } else {
+    // No cookie or incorrect hash in cookie. Redirect to login.
+    return new Response(getTemplate({ withError: error === '1' }), {
+      headers: {
+        'content-type': 'text/html'
+      }
+    });
+  }
+}
